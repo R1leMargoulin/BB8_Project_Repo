69fcbd3326e385ea259ce2366e0ba2287cb192e7
R1 <erwan.martin@viacesi.fr>
2024-02-18 14:45:46 +0100

    add joystick realtime, still need to readjuste joystock directions

--- test/interface_web/server.py
+++ test/interface_web/server.py
@@ -1,17 +1,21 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+import ast
 
 app = Flask(__name__)
 
 @app.route('/',methods=['POST'])
 def query_example():
+    data = "{"+ ast.literal_eval(str(request.get_data(), encoding="utf-8"))["body"]+"}"
+    data = ast.literal_eval(data)
+    print(f"speed : {data['speed']}")
+    print(f"turn : {data['angle']}")
     return request.get_data()
 
 @app.after_request
 def after(response):
     # todo with response
-    print(response.get_data())
     return response
 
 if __name__ == '__main__':
     # run app in debug mode on port 5000
-    app.run(debug=True, port=5000)
+    app.run(host="192.168.4.1", debug=False, port=5000)
